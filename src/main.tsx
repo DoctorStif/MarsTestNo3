e8a523b2ef2e53d9201899fc20096fece4383bef
Furkan <furkan@users.noreply.github.com>
2026-06-06 21:10:25 +0300

    Configure GitHub Pages base path

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import i18n from './i18n'
 import './index.css'
 import App from './App.tsx'
 
+const routerBaseName = import.meta.env.BASE_URL.replace(/\/$/, '')
+
 window.marsReactI18nChangeLanguage = (language: string) => {
   void i18n.changeLanguage(language)
 }
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <BrowserRouter basename={routerBaseName || undefined}>
     <App />
   </BrowserRouter>,
 )
